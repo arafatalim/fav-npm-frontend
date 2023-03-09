099e1d79f7e5f1811790d5ec59b185cd4fcd0465
arafatalim <arafat@betalectic.com>
2023-03-09 19:09:30 +0530

    FIx the error, by adding babelsrc file and updating the eslint file

---
+++ src/pages/member/login.js
@@ -0,0 +1,12 @@
+
+
+const Login = () => {
+      return (
+            <div>
+                  <h1>Login Here</h1>
+                  <p>Enter your Details </p>
+            </div>
+      )
+}
+
+export default Login;
---
+++ src/pages/member/register.js
@@ -0,0 +1,7 @@
+function Register(){
+      return (
+            <div>
+                  <h1>Hello</h1>
+            </div>
+      )
+}
