7a4766440e9283e93e9cae00031c02598fb581eb
arafatalim <arafat@betalectic.com>
2023-03-10 12:40:25 +0530

    Tailwind and Reusejs configuration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import { ThemeProvider, theme } from '@reusejs/react';
+// import '@/styles/tailwind.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <ThemeProvider value={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
 }
